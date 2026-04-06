ed952e554d367dc1c013e86087aab134bb9ab641
dulavi80maz <dulavi80maz@umes.edu.gt>
2026-04-06 12:57:26 -0600

    Add files via upload

---
+++ Ejercicios if.py
@@ -0,0 +1,18 @@
+horas_sueno=int(input("Cuantas horas dormiste?:"))
+cafe=input ("Tomaste cafe?: (si/no)")
+examen=input("Tines examen hoy?: (si/no)")
+
+if horas_sueno>=7
+    print ("Estas fresh, sobrevives sin problemas")
+else:
+    if cafe== "si":
+        print ("Sobrevives con el poder del cafe")
+else:
+    if examen== "si":
+        print ("No sobreviviste!!!")
+else:
+    if examen== "si":
+        print ("Sobrevives...PERO, como ZOMBIE!")
+
+
+
