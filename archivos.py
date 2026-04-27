e4fa04264892dc96f0914575124ba383151734c8
dulavi80maz <dulavi80maz@umes.edu.gt>
2026-04-27 12:17:04 -0600

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -1,1 +1,12 @@
+#Usando archivos
+#open(nombre_archivo, modo)
+#nombre_archivo: indicar nombre y tipo
+#modo: "r" leer, w "sobreescribir"
+#"a" agrega al final, "r+" lee y escribe sin borrar el contenido 
+#"a+" lee y agrega al final 
+
+#Escribir
+with open("datos.txt" , "w")
+archivo.write("Hola Mundo")
+ 
 
